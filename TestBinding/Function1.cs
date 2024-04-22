b09e394701b4e36139dce18c8ef6ebda3707bf61
camalzadeelvin <camalzadeelvin@STHQ0118-01>
2024-04-22 09:53:41 +0400

    Add project files.

---
+++ TestBinding/Function1.cs
@@ -0,0 +1,26 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+namespace TestBinding
+{
+    public static class Function1
+    {
+        [FunctionName("Function1")]
+        [return: Table("Products", Connection = "MyAzureStorage")]
+        public static async Task<Product> Run(
+            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+            string requestBody=await new StreamReader(req.Body).ReadToEndAsync();
+            Product newProduct=JsonConvert.DeserializeObject<Product>(requestBody);
+            return newProduct;
+        }
+    }
+}
